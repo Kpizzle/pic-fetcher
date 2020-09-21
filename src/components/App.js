0db242772d69dd74c4938ea0c38c39ab70a406bf
Kyle Potter <kp1289@gmail.com>
2020-09-21 21:53:53 +0100

    added axios, made requests and displayed a result

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 export class App extends React.Component {
-    onSearchSubmit(term){
-        console.log(term)
-    }
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID W1GUkcs4D1bqXJGt3K83S-uttmc4SL3cclWTPsIogGc ",
+      },
+    });
+    this.setState({ images: response.data.results });
+  };
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
