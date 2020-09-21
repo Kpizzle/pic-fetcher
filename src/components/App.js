a746b2b021e8318b2ca95af49a46941af552d6d3
Kyle Potter <kp1289@gmail.com>
2020-09-21 22:02:01 +0100

    added axios and refactored into helper

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import axios from "axios";
+import unsplash from '../api/unsplash'
 import SearchBar from "./SearchBar";
+
 
 export class App extends React.Component {
   state = { images: [] };
 
   onSearchSubmit = async (term) => {
-    const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+    const response = await unsplash.get(`/search/photos`, {
       params: { query: term },
-      headers: {
-        Authorization: "Client-ID W1GUkcs4D1bqXJGt3K83S-uttmc4SL3cclWTPsIogGc ",
-      },
+
     });
     this.setState({ images: response.data.results });
   };
