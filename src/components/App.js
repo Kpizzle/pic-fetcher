24d7f834b379e34c1942be097e6fe29df36d0a74
Kyle Potter <kp1289@gmail.com>
2020-09-21 22:36:32 +0100

    added displaying components as lists

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import unsplash from '../api/unsplash'
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
-
+import ImageList from "./ImageList";
 
 export class App extends React.Component {
   state = { images: [] };
@@ -9,7 +9,6 @@ export class App extends React.Component {
   onSearchSubmit = async (term) => {
     const response = await unsplash.get(`/search/photos`, {
       params: { query: term },
-
     });
     this.setState({ images: response.data.results });
   };
@@ -19,6 +18,9 @@ export class App extends React.Component {
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
         Found: {this.state.images.length} images
+        <div>
+          <ImageList images={this.state.images} />
+        </div>
       </div>
     );
   }
